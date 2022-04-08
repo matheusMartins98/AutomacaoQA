3a34732e80e2a6b454d1a63d267ce1581625b611
Matheus de Oliveira Martins <matheus.martins@castecnologia.com.br>
2022-04-08 14:39:51 -0300

    estrutura

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@SuppressWarnings("deprecation")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "test.features",
+        glue = "steps",
+        plugin = {"html:target/cucumber-report, pretty"},
+        snippets = SnippetType.CAMELCASE,
+        monochrome = true
+        )   
+public class TestRunner {
+	
+}
+ 
